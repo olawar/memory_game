16d8e979756694f4881015ddb4a214220538deab
olawar <warzecha.ola@gmail.com>
2016-06-26 17:02:37 +0200

    Initial commit

---
+++ src/js/scripts.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Awar on 2016-06-26.
+ */
+var app = {
+    init: function() {
+        this.memoGame();
+    },
+    memoGame: function() {
+
+        console.log('test');
+
+
+
+    }
+};
+
+
+$(document).ready(function() {
+    app.init();
+});
