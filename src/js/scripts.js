79ad57460a9fc813976c2e05a03569366226fccf
olawar <warzecha.ola@gmail.com>
2016-07-18 22:48:59 +0200

    Improvements to game logic

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,63 +3,59 @@
  */
 'use strict';
 
+// passing options of the game - commented variants will let you try out different options quickly
+
+var options = {
+    "tiles_in_a_row": 6,
+    "tiles_in_a_column": 6,
+    "preferred_tile_size": 200,
+    "space_between_tiles": 10,
+    "background": "bg/treeson_blue.jpg",
+    // "background": "",
+    // "tiles_images_source_type": "server", // "local as another option"
+    // "tiles_images_source": "http://www.warzecha.org/ola/memory/images/",
+    // "tiles_images_json": "http://www.warzecha.org/ola/memory/enumpic.php" // my own source serving JSON - if you choose local, leave an empty string or do not use it at all
+    "tiles_images_source_type": "local",
+    "tiles_images_source": "images/"
+};
+
 var app = {
     init: function() {
-        this.memoGame();
+        this.setGameboard();
+        this.memoPlay();
     },
-    memoGame: function() {
-
-        // passing options of the game - commented variants will let you try out different options quickly
-
-        var options = {
-            "tiles_in_a_row": 5,
-            "tiles_in_a_column": 6,
-            "preferred_tile_size": 200,
-            "space_between_tiles": 10,
-            "background": "bg/treeson_blue.jpg",
-            // "background": "",
-            "tiles_images_source_type": "server", // "local as another option"
-            "tiles_images_source": "http://www.warzecha.org/ola/memory/images/",
-            "tiles_images_json": "http://www.warzecha.org/ola/memory/enumpic.php" // my own source serving JSON - if you choose local, leave empty or do not use it at all
-            // "tiles_images_source_type": "local",
-            // "tiles_images_source": "images/"
-        };
-
-        // setting the gameboard
-
+    setGameboard: function() {
         var gameboard = $('[data-game]');
-        setGameboard();
-
-        // in DOM - based on options
-
-        function setGameboard() {
-            if ((options.tiles_in_a_column * options.tiles_in_a_row) % 2 > 0) {
-                // the number of tiles cannot be odd, so we need to deal with this here
-                for (var i = 0; i < (options.tiles_in_a_column * (options.tiles_in_a_row - 1)); i++) {
-                    gameboard.append('<div class="tile"></div>');
-                    $('.tile').css({
-                        'width': options.preferred_tile_size + 'px',
-                        'max-width': "calc(100% / " + options.tiles_in_a_row + " - 2 * " + options.space_between_tiles + "px )",
-                        'flex-basis': "calc(100% / " + options.tiles_in_a_row + " - 2 * " + options.space_between_tiles + "px )",
-                        'margin': options.space_between_tiles + 'px'
-                    })
-                }
-            } else {
-                for (var i = 0; i < (options.tiles_in_a_column * options.tiles_in_a_row); i++) {
-                    gameboard.append('<div class="tile"></div>');
-                    $('.tile').css({
-                        'width': options.preferred_tile_size + 'px',
-                        'max-width': "calc(100% / " + options.tiles_in_a_row + " - 2 * " + options.space_between_tiles + "px )",
-                        'flex-basis': "calc(100% / " + options.tiles_in_a_row + " - 2 * " + options.space_between_tiles + "px )",
-                        'margin': options.space_between_tiles + 'px'
-                    })
-                }
+        if ((options.tiles_in_a_column * options.tiles_in_a_row) % 2 > 0) {
+            // the number of tiles cannot be odd, so we need to deal with this here
+            for (var i = 0; i < (options.tiles_in_a_column * (options.tiles_in_a_row - 1)); i++) {
+                gameboard.append('<div class="tile"></div>');
+                $('.tile').css({
+                    'width': options.preferred_tile_size + 'px',
+                    'max-width': "calc(100% / " + options.tiles_in_a_row + " - 2 * " + options.space_between_tiles + "px )",
+                    'flex-basis': "calc(100% / " + options.tiles_in_a_row + " - 2 * " + options.space_between_tiles + "px )",
+                    'margin': options.space_between_tiles + 'px'
+                })
+            }
+        } else {
+            for (var i = 0; i < (options.tiles_in_a_column * options.tiles_in_a_row); i++) {
+                gameboard.append('<div class="tile"></div>');
+                $('.tile').css({
+                    'width': options.preferred_tile_size + 'px',
+                    'max-width': "calc(100% / " + options.tiles_in_a_row + " - 2 * " + options.space_between_tiles + "px )",
+                    'flex-basis': "calc(100% / " + options.tiles_in_a_row + " - 2 * " + options.space_between_tiles + "px )",
+                    'margin': options.space_between_tiles + 'px'
+                })
             }
         }
+
+        // setting gameboard background image if available
 
         if (options.background !== "") {
             gameboard.css('background-image', 'url("' + options.background + '")');
         }
+    },
+    memoPlay: function() {
 
         // getting the right images & randomizing them
 
@@ -145,53 +141,59 @@ var app = {
         $(document).on("ajaxStop", function() {
 
             $('.tile').on('click', function() {
-                if ($(this).hasClass('inactive') || $(this).hasClass('invisible')) {
+                var _tile = $(this),
+                    _flipped = $('.flipped');
+                if (_tile.hasClass('inactive') || _tile.hasClass('invisible')) {
                     return false;
                 } else {
-                    $(this).addClass('flipped');
-                    $(this).css('background-image', 'url("' + options.tiles_images_source + gameSetup[$(this).attr('id')][1] + '")');
-
-                    if ($('.tile-first').length) {
-                        if ($(this).css('background-image') === $('.tile-first').css('background-image')) {
-
-                            $(this).addClass('tile-second').addClass('right');
-                            $('.tile-first').addClass('right');
-
-                            setTimeout(function() {
-                                disable();
-                            }, 800);
-
-                        } else {
-                            $(this).addClass('tile-second').addClass('wrong');
-                            $('.tile-first').addClass('wrong');
-
-                            setTimeout(function() {
-                                flipBack();
-                            }, 800);
-                        }
-                    } else {
-                        $(this).addClass('tile-first');
-                    }
+                    _tile.addClass('flipped').css('background-image', 'url("' + options.tiles_images_source + gameSetup[$(this).attr('id')][1] + '")');
+                   checkFlipped();
                 }
             });
 
         });
 
+        // checking if we have a pair
+        
+        function checkFlipped() {
+            var _flipped = $('.flipped');
+            if (_flipped.length == 2) {
+                if (_flipped.eq(0).css('background-image') === _flipped.eq(1).css('background-image')) {
+                    _flipped.eq(0).addClass('right');
+                    _flipped.eq(1).addClass('right');
+                    setTimeout(function() {
+                        disable();
+                    }, 800);
+                } else {
+                    _flipped.eq(0).addClass('wrong');
+                    _flipped.eq(1).addClass('wrong');
+                    setTimeout(function() {
+                        flipBack();
+                    }, 800);
+                }
+                return;
+            }
+            else if (_flipped.length > 2) {
+                return;
+            }
+        }
+
         // callback for disabling right tiles
 
         function disable() {
             if (options.background !== "") {
-                $('.tile-first, .tile-second').removeClass().addClass('tile').addClass('invisible');
+                $('.right').attr('class', 'tile invisible');
             } else {
-                $('.tile-first, .tile-second').removeClass().addClass('tile').addClass('inactive');
+                $('.right').attr('class', 'tile inactive');
             }
         }
 
         // callback for flipping back wrong tiles
 
         function flipBack() {
-            $('.tile-first, .tile-second').removeClass().addClass('tile').css('background-image', "");
+            $('.wrong').attr('class', 'tile').css('background-image', "");
         }
+
     }
 };
 
